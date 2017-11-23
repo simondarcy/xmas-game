7e2a662b14a0a4d6983f09752e1b9506e23ebef9
sidarcy <simon.darcy@rte.ie>
2017-11-23 22:20:07 +0000

    Add flip orientation stuff

--- js/main.js
+++ js/main.js
@@ -6,10 +6,73 @@ Percent = function(num, dir){
     return p;
 };
 
+var game;
 
-var game = new Phaser.Game(w, h, Phaser.AUTO, 'game');
-game.state.add('Preloader', Preloader);
-game.state.add('Menu', Menu);
-game.state.add('Game', Game);
-game.state.add('GameOver', GameOver);
-game.state.start('Preloader');
+
+function _isMobile(){
+    // if we want a more complete list use this: http://detectmobilebrowsers.com/
+    // str.test() is more efficent than str.match()
+    // remember str.test is case sensitive
+    var isMobile = (/iphone|ipod|android|ie|blackberry|fennec/).test
+    (navigator.userAgent.toLowerCase());
+    return isMobile;
+}
+
+
+function init() {
+    game = new Phaser.Game(w, h, Phaser.AUTO, 'game');
+    game.state.add('Preloader', Preloader);
+    game.state.add('Menu', Menu);
+    game.state.add('Game', Game);
+    game.state.add('GameOver', GameOver);
+    game.state.start('Preloader');
+}
+
+
+var flipped = false;
+
+function doOnOrientationChange()
+{
+    //switch statement to select a behaviour based on the screens orientation
+    switch (window.orientation) {
+        case 0:
+        case 180:
+            //Portrait mode: show the illustration
+            console.log("please filip");
+            if (!flipped) {
+                document.getElementById("flip").style.display = "block";
+            }
+            break;
+        case 90:
+        case -90:
+            //Lanscape mode: hide the illustration
+            document.getElementById("flip").style.display = "none";
+            //prevent re init should user flip multiple times
+            if (!flipped) {
+                init();
+                flipped = true;
+            }
+            break;
+        case undefined:
+
+            if(navigator.userAgent.indexOf("Chrome") == -1) {
+                init();
+            }
+
+
+    }
+}
+
+window.onload = function()
+{
+    if (_isMobile()) {
+
+        doOnOrientationChange();
+        //detect orientation change
+        window.addEventListener('orientationchange', doOnOrientationChange);
+    }
+    else{
+        init();
+    }
+
+};
